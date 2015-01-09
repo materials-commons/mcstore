279fa019910cfd67670b50e48a76f9ef1b6aaf73
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-09 14:41:16 -0500

    Add additional ways to get a rethinkdb session. Add methods that will panic if a session cannot be allocated.

--- pkg/db/rsession.go
+++ pkg/db/rsession.go
@@ -13,3 +13,32 @@ func RSession() (*r.Session, error) {
 			Database: config.GetString("MCDB_NAME"),
 		})
 }
+
+// RSessionMust creates a new RethinkDB session and panics if it cannot
+// allocate it.
+func RSessionMust() *r.Session {
+	session, err := RSession()
+	if err != nil {
+		panic("Couldn't get new rethinkdb session")
+	}
+	return session
+}
+
+// RSessionUsing createa new RethinkDB session using the passed in parameters
+func RSessionUsing(address, db string) (*r.Session, error) {
+	return r.Connect(
+		r.ConnectOpts{
+			Address:  address,
+			Database: db,
+		})
+}
+
+// RSessionUsingMust creates a new RethinkDB session and panics if it cannot
+// allocate it.
+func RSessionUsingMust(address, db string) *r.Session {
+	session, err := RSessionUsing(address, db)
+	if err != nil {
+		panic("Couldn't get new rethinkdb session")
+	}
+	return session
+}
