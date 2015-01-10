5e89dbb60045756b25419c31fd2edae120e78bc3
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-01-09 23:09:28 -0500

    Remove unneeded files.

--- pkg/domain/interfaces.go
+++
@@ -1,7 +0,0 @@
-package domain
-
-import "github.com/materials-commons/mcfs/base/schema"
-
-type Validator interface {
-	HasAccess(userID, fileID string) (schema.File, error)
-}
--- pkg/domain/rvalidator.go
+++
@@ -1,4 +0,0 @@
-package domain
-
-type rValidator struct {
-}
