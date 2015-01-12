9f8484713e2ad23cd23fbd8d58b2fe67f2564dc8
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-12 14:50:21 -0500

    Add Mock for Access so we can test without have to mock out the other dai depdendencies.

---
+++ pkg/domain/mocks/Access.go
@@ -0,0 +1,30 @@
+package mocks
+
+import "github.com/stretchr/testify/mock"
+
+import "github.com/materials-commons/mcstore/pkg/db/schema"
+
+type Access struct {
+	mock.Mock
+}
+
+func NewMAccess() *Access {
+	return &Access{}
+}
+
+func (m *Access) AllowedByOwner(owner, user string) bool {
+	ret := m.Called(owner, user)
+
+	r0 := ret.Get(0).(bool)
+
+	return r0
+}
+
+func (m *Access) GetFile(apikey, fileID string) (*schema.File, error) {
+	ret := m.Called(apikey, fileID)
+
+	r0 := ret.Get(0).(*schema.File)
+	r1 := ret.Error(1)
+
+	return r0, r1
+}
