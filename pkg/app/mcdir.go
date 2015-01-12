5f8e1ea92a752ad70565e1b2a5e1a7afae021b69
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-12 11:49:54 -0500

    Add unit tests to mcdir and tighten up the error checking.

--- pkg/app/mcdir.go
+++ pkg/app/mcdir.go
@@ -13,14 +13,31 @@ type mcdir struct{}
 var MCDir mcdir
 
 func (d mcdir) Path() string {
-	return config.GetString("MCDIR")
+	dir := config.GetString("MCDIR")
+	if dir == "" {
+		panic("MCDIR not set")
+	}
+	return dir
 }
 
 func (d mcdir) FileDir(fileID string) string {
 	idSegments := strings.Split(fileID, "-")
-	return filepath.Join(d.Path(), idSegments[1][0:2], idSegments[1][2:4])
+	switch {
+	case len(idSegments) < 2:
+		Log.Debug(Logf("FileDir: Got bad fileID: %s", fileID))
+		return ""
+	case len(idSegments[1]) < 4:
+		Log.Debug(Logf("FileDir: Got bad fileID: %s", fileID))
+		return ""
+	default:
+		return filepath.Join(d.Path(), idSegments[1][0:2], idSegments[1][2:4])
+	}
 }
 
 func (d mcdir) FilePath(fileID string) string {
+	dir := d.FileDir(fileID)
+	if dir == "" {
+		return ""
+	}
 	return filepath.Join(d.FileDir(fileID), fileID)
 }
---
+++ pkg/app/mcdir_test.go
@@ -0,0 +1,53 @@
+package app
+
+import (
+	"testing"
+
+	"github.com/materials-commons/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPath(t *testing.T) {
+	// Test MCDIR not set
+	config.Set("MCDIR", "")
+	panicFunc := func() {
+		MCDir.Path()
+	}
+	require.Panics(t, panicFunc, "MCDIR not set and didn't panic")
+	config.Set("MCDIR", "/tmp/mcdir")
+	require.Equal(t, MCDir.Path(), "/tmp/mcdir", "Expected MCDir.Path to return /tmp/mcdir")
+}
+
+func TestFileDir(t *testing.T) {
+	// Good file id
+	fileID := "abc-defg-ghi-jkl-mnopqr"
+	dir := MCDir.FileDir(fileID)
+	require.Equal(t, "/tmp/mcdir/de/fg", dir, "Expected /tmp/mcdir/de/fg got %s", dir)
+
+	// Bad file id
+	fileID = "bad_file_id"
+	dir = MCDir.FileDir(fileID)
+	require.Equal(t, "", dir, "Expected '', got %s", dir)
+
+	// Bad segment in file id
+	fileID = "abc-def-ghi-jkl"
+	dir = MCDir.FileDir(fileID)
+	require.Equal(t, "", dir, "Expected '', got %s", dir)
+}
+
+func TestFilePath(t *testing.T) {
+	// Good file id
+	fileID := "abc-defg-ghi-jkl-mnopqr"
+	path := MCDir.FilePath(fileID)
+	require.Equal(t, "/tmp/mcdir/de/fg/abc-defg-ghi-jkl-mnopqr", path, "Expected /tmp/mcdir/de/fg/abc-defg-ghi-jkl-mnopqr, got %s", path)
+
+	// Bad file id
+	fileID = "bad_file_id"
+	path = MCDir.FilePath(fileID)
+	require.Equal(t, "", path, "Expected '', got %s", path)
+
+	// Bad segment in file id
+	fileID = "abc-def-ghi-jkl"
+	path = MCDir.FilePath(fileID)
+	require.Equal(t, "", path, "Expected '', got %s", path)
+}
