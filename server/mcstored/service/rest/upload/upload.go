a71767d6b813c12ee0145361050a8417c3f6a9f6
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-13 16:45:37 -0500

    Remove unused code. General cleanup and pass a real request writer.

--- server/mcstored/service/rest/upload/upload.go
+++ server/mcstored/service/rest/upload/upload.go
@@ -1,16 +1,12 @@
 package upload
 
 import (
-	"net/http"
-
 	"github.com/emicklei/go-restful"
 	"github.com/inconshreveable/log15"
 	"github.com/materials-commons/mcstore/pkg/app"
 	"github.com/materials-commons/mcstore/pkg/db/schema"
 	"github.com/materials-commons/mcstore/pkg/ws/rest"
 )
-
-const chunkPerms = 0700 // Permissions to set uploads to
 
 // An uploadResource handles all upload requests.
 type uploadResource struct {
@@ -21,7 +17,7 @@ type uploadResource struct {
 // NewResources creates a new upload resource
 func NewResource() rest.Service {
 	return &uploadResource{
-		uploader: newUploader(nil),
+		uploader: newUploader(newFileRequestWriter(newMCDirRequestPath())),
 		log:      app.NewLog("resource", "upload"),
 	}
 }
@@ -30,22 +26,12 @@ func NewResource() rest.Service {
 func (r *uploadResource) WebService() *restful.WebService {
 	ws := new(restful.WebService)
 
-	ws.Path("/upload").
-		Produces(restful.MIME_JSON)
+	ws.Path("/upload").Produces(restful.MIME_JSON)
 	ws.Route(ws.POST("/chunk").To(rest.RouteHandler1(r.uploadFileChunk)).
 		Consumes("multipart/form-data").
 		Doc("Upload a file chunk"))
-	// ws.Route(ws.GET("/chunk").To(rest.RouteHandler(r.testFileChunk)).
-	// 	Reads(flow.Request{}).
-	// 	Doc("Test if chunk already uploaded."))
 
 	return ws
-}
-
-// testFileChunk checks if a chunk has already been uploaded. At the moment we don't
-// support this functionality, so always return an error.
-func (r *uploadResource) testFileChunk(request *restful.Request, response *restful.Response) {
-	response.WriteErrorString(http.StatusInternalServerError, "no such file")
 }
 
 // uploadFileChunk uploads a new file chunk.
