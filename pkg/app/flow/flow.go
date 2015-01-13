4341d2a718d2cc29d28e50d4f2e398e09fe0789a
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-13 16:05:03 -0500

    Remove write functionality. Put writing a request behind a set of interfaces so we can test it without having to use a file system.

--- pkg/app/flow/flow.go
+++ pkg/app/flow/flow.go
@@ -1,13 +1,6 @@
 package flow
 
-import (
-	"fmt"
-	"io/ioutil"
-	"os"
-	"path/filepath"
-
-	"github.com/materials-commons/mcstore/pkg/app"
-)
+import "fmt"
 
 const ChunkPerms = 0700
 
@@ -33,49 +26,4 @@ type Request struct {
 
 func (r *Request) UploadID() string {
 	return fmt.Sprintf("%s-%s-%s", r.ProjectID, r.DirectoryID, r.FileID)
-}
-
-func (r *Request) Write() error {
-	path := r.Path()
-	err := r.validateWrite(path)
-	switch {
-	case err == nil:
-		return ioutil.WriteFile(path, r.Chunk, ChunkPerms)
-	case err == app.ErrExists:
-		return nil
-	default:
-		return err
-	}
-}
-
-// validateWrite determines if a particular chunk can be written.
-// If the size of the ondisk chunk is smaller than the request
-// chunk then that chunk is incomplete and we allow a write to it.
-func (r *Request) validateWrite(path string) error {
-	if err := os.MkdirAll(path, ChunkPerms); err != nil {
-		return err
-	}
-	finfo, err := os.Stat(path)
-	switch {
-	case os.IsNotExist(err):
-		return nil
-	case err != nil:
-		return app.ErrInvalid
-	case finfo.Size() < int64(r.FlowChunkSize):
-		return nil
-	case finfo.Size() == int64(r.FlowChunkSize):
-		return app.ErrExists
-	default:
-		return app.ErrInvalid
-	}
-}
-
-func (r *Request) Path() string {
-	return filepath.Join(r.Dir(), fmt.Sprintf("%d", r.FlowChunkNumber))
-}
-
-func (r *Request) Dir() string {
-	mcdir := app.MCDir.Path()
-	uploadPath := filepath.Join(mcdir, "upload", r.ProjectID, r.DirectoryID, r.FileID)
-	return uploadPath
 }
