2e45d763158e29abd1011044cc482b34680e196c
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-01-13 21:35:00 -0500

    Playing around with code to figure out what dependencies need to be injected.

--- server/mcstored/service/rest/upload/uploader.go
+++ server/mcstored/service/rest/upload/uploader.go
@@ -1,18 +1,29 @@
 package upload
 
-import "github.com/materials-commons/mcstore/pkg/app/flow"
+import (
+	"io"
+	"os"
+
+	"github.com/materials-commons/mcstore/pkg/app/flow"
+)
 
 // TODO: Uploader will need to know the destination for assembly.
+// TODO: Uploader will need a Finisher to be passed in to it.
+// TODO: ***** To simplify the above, just pass in the assembler *****
 
 type uploader struct {
-	tracker *uploadTracker
-	w       RequestWriter
+	tracker     *uploadTracker
+	w           RequestWriter
+	finisher    Finisher
+	destination io.Writer
 }
 
-func newUploader(w RequestWriter) *uploader {
+func newUploader(w RequestWriter, destination io.Writer, finisher Finisher) *uploader {
 	return &uploader{
-		tracker: newUploadTracker(),
-		w:       w,
+		tracker:     newUploadTracker(),
+		w:           w,
+		finisher:    finisher,
+		destination: destination,
 	}
 }
 
@@ -35,26 +46,28 @@ func (u *uploader) uploadDone(request *flow.Request) bool {
 }
 
 func (u *uploader) assembleUpload(request *flow.Request) {
-	assembler := NewAssembler(nil, nil) // fix this with real items and finisher
+	assembler := NewAssembler(nil, u.finisher) // Need list of Items
 	go func() {
-		assembler.To(nil) // fix this with real destination
+		assembler.To(u.destination) // fix this with real destination
 	}()
 }
 
 type uploadFinisher struct {
-	uploadID string
-	tracker  *uploadTracker
+	uploadID  string
+	tracker   *uploadTracker
+	uploadDir string
 }
 
-func newUploadFinisher(uploadID string, tracker *uploadTracker) *uploadFinisher {
+func newUploadFinisher(uploadID string, tracker *uploadTracker, uploadDir string) *uploadFinisher {
 	return &uploadFinisher{
-		uploadID: uploadID,
-		tracker:  tracker,
+		uploadID:  uploadID,
+		tracker:   tracker,
+		uploadDir: uploadDir,
 	}
 }
 
 func (f *uploadFinisher) Finish() error {
 	f.tracker.clear(f.uploadID)
-	//os.RemoveAll(uploadDir)
+	os.RemoveAll(f.uploadDir)
 	return nil
 }
