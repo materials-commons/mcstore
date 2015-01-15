b8dce56ca51d2b719f5cca6e95fad4c7305e54fd
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-15 14:52:44 -0500

    Remove UploadAssembler interface (unused), add comments.

--- server/mcstored/service/rest/upload/request.go
+++ server/mcstored/service/rest/upload/request.go
@@ -10,29 +10,32 @@ import (
 	"github.com/materials-commons/mcstore/pkg/app/flow"
 )
 
+// RequestWriter is the interface used to write a request.
 type RequestWriter interface {
 	Write(req *flow.Request) error
 }
 
-type UploadAssembler interface {
-	Assemble(uploadID string)
-}
-
+// RequestPath is the interface for getting request locations.
 type RequestPath interface {
 	Path(req *flow.Request) string
 	Dir(req *flow.Request) string
 }
 
+// A fileRequestWriter implements writing a request to a file.
 type fileRequestWriter struct {
 	RequestPath
 }
 
+// NewFileRequestWriter creates a new fileRequestWriter.
 func NewFileRequestWriter(requestPath RequestPath) *fileRequestWriter {
 	return &fileRequestWriter{
 		RequestPath: requestPath,
 	}
 }
 
+// Write will write the blocks for a request to the path returned by
+// the RequestPath Path call. Write will attempt to create the directory
+// path to write to.
 func (r *fileRequestWriter) Write(req *flow.Request) error {
 	path := r.Path(req)
 	err := r.validateWrite(path, req)
@@ -68,19 +71,27 @@ func (r *fileRequestWriter) validateWrite(path string, req *flow.Request) error 
 	}
 }
 
+// mcdirRequestPath implements RequestPath by returning paths starting
+// from MCDIR. It gets the dir path from app.MCDir.Path().
 type mcdirRequestPath struct{}
 
+// NewMCDirRequestPath creates a new mcdirRequestPath.
 func NewMCDirRequestPath() *mcdirRequestPath {
 	return &mcdirRequestPath{}
 }
 
+// Path returns the full file path for a request. The path is constructed
+// from app.MCDir.Path() and the request FlowChunkNumber. This allows the
+// blocks for a file upload to be sorted so the file can be constructed.
 func (p *mcdirRequestPath) Path(req *flow.Request) string {
 	return filepath.Join(p.Dir(req), fmt.Sprintf("%d", req.FlowChunkNumber))
 }
 
+// Dir returns the path to put the request blocks to. The path is constructed
+// from app.MCDir.Path and the request.UploadID().
 func (p *mcdirRequestPath) Dir(req *flow.Request) string {
 	mcdir := app.MCDir.Path()
-	uploadPath := filepath.Join(mcdir, "upload", req.ProjectID, req.DirectoryID, req.FileID)
+	uploadPath := filepath.Join(mcdir, "upload", req.UploadID())
 	return uploadPath
 }
 
