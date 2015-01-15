53153f640500f477e4a14d65144ae59edb9fd87d
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-15 16:41:50 -0500

    Add Insert to rfiles.

--- pkg/db/dai/rfiles.go
+++ pkg/db/dai/rfiles.go
@@ -2,6 +2,7 @@ package dai
 
 import (
 	r "github.com/dancannon/gorethink"
+	"github.com/materials-commons/mcstore/pkg/app"
 	"github.com/materials-commons/mcstore/pkg/db/model"
 	"github.com/materials-commons/mcstore/pkg/db/schema"
 )
@@ -25,4 +26,45 @@ func (f rFiles) ByID(id string) (*schema.File, error) {
 		return nil, err
 	}
 	return &file, nil
+}
+
+// Insert adds a new file to the system.
+func (f rFiles) Insert(file *schema.File, dirID string, projectID string) (*schema.File, error) {
+	var newFile schema.File
+	if err := model.Files.Qs(f.session).Insert(file, &newFile); err != nil {
+		return nil, err
+	}
+
+	err := f.updateDependencies(newFile.ID, dirID, projectID)
+	return &newFile, err
+}
+
+// updateDependencies updates the datadir2datafile and project2datafile tables.
+func (f rFiles) updateDependencies(fileID, dirID, projectID string) error {
+	proj2file := schema.Project2DataFile{
+		ProjectID:  projectID,
+		DataFileID: fileID,
+	}
+
+	dir2file := schema.DataDir2DataFile{
+		DataDirID:  dirID,
+		DataFileID: fileID,
+	}
+
+	err1 := model.Files.Qs(f.session).InsertRaw("datadir2datafile", &dir2file, nil)
+	app.Log.Error(app.Logf("Unable to update datadir2datafile for file %s, directory %s, error %s", fileID, dirID, err1))
+
+	err2 := model.Files.Qs(f.session).InsertRaw("project2datafile", &proj2file, nil)
+	app.Log.Error(app.Logf("Unable to update project2datafile for file %s, project %s, error %s", fileID, projectID, err2))
+
+	// We will return one of the errors, even if both inserts failed. There really isn't anything the
+	// system can do about a specific error, so we just want to communicate that a failure occured.
+	switch {
+	case err1 != nil:
+		return err1
+	case err2 != nil:
+		return err2
+	default:
+		return nil
+	}
 }
