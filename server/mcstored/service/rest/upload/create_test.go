546d7d1e886d1b602a31047ba80f9aa161983303
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-22 14:58:39 -0500

    Tests for creating an upload request.

---
+++ server/mcstored/service/rest/upload/create_test.go
@@ -0,0 +1,15 @@
+package upload
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateUploadRequest(t *testing.T) {
+	require.True(t, false, "Not implemented")
+}
+
+func TestGetExistingUploads(t *testing.T) {
+	require.True(t, false, "Not implemented")
+}
