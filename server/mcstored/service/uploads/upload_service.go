97402a338e80451f66bc48d6f9a9f55e2a533e60
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-29 14:10:32 -0500

    Test syste. Clean up on errors. Add extra logging.

--- server/mcstored/service/uploads/upload_service.go
+++ server/mcstored/service/uploads/upload_service.go
@@ -1,10 +1,12 @@
 package uploads
 
 import (
+	"io"
 	"os"
 
 	"github.com/materials-commons/mcstore/pkg/app"
 	"github.com/materials-commons/mcstore/pkg/app/flow"
+	"github.com/materials-commons/mcstore/pkg/db"
 	"github.com/materials-commons/mcstore/pkg/db/dai"
 	"github.com/materials-commons/mcstore/pkg/db/schema"
 )
@@ -25,16 +27,16 @@ type uploadService struct {
 }
 
 func NewUploadService() *uploadService {
-	return &uploadService{}
+	session := db.RSessionMust()
+	return &uploadService{
+		tracker: newUploadTracker(),
+		files:   dai.NewRFiles(session),
+		uploads: dai.NewRUploads(session),
+	}
 }
 
 func (s *uploadService) Upload(req *UploadRequest) error {
 	dir := s.requestDir(req.Request)
-
-	if s.allBlocksUploaded(req.UploadID(), req.FlowTotalChunks) {
-		s.assemble(req, dir)
-		return nil
-	}
 
 	if err := s.Write(dir, req.Request); err != nil {
 		return err
@@ -42,6 +44,17 @@ func (s *uploadService) Upload(req *UploadRequest) error {
 
 	id := req.UploadID()
 	s.tracker.increment(id)
+
+	if s.allBlocksUploaded(id, req.FlowTotalChunks) {
+		if file, err := s.assemble(req, dir); err != nil {
+			if file != nil {
+				err2 := s.cleanup(req, file.ID)
+				app.Log.Errorf("Assembly failed for uploaded file: attempted cleanup of database entry returned: %s", err2)
+			}
+			return err
+		}
+	}
+
 	return nil
 }
 
@@ -50,32 +63,40 @@ func (s *uploadService) allBlocksUploaded(id string, totalChunks int32) bool {
 	return count == totalChunks
 }
 
-func (s *uploadService) assemble(req *UploadRequest, dir string) {
+func (s *uploadService) assemble(req *UploadRequest, dir string) (*schema.File, error) {
+	// Create file entry in database
 	file, err := s.createFile(req)
 	if err != nil {
-		// log
-		return
+		app.Log.Errorf("Assembly failed for request %s, couldn't create file in database: %s", req.FlowIdentifier, err)
+		return nil, err
 	}
 
-	dest, err := os.Create(app.MCDir.FilePath(file.ID))
+	// Create on disk entry to write chunks to
+	dest, err := s.createDest(file.ID)
 	if err != nil {
-		// log
-		return
+		app.Log.Errorf("Assembly failed for request %s, couldn't create file on disk: %s", req.FlowIdentifier, err)
+		return file, err
 	}
 
+	// Assemble the chunks
 	chunkSupplier := newDirChunkSupplier(dir)
 	if err := assembleRequest(chunkSupplier, dest); err != nil {
-		// log
-		return
+		app.Log.Errorf("Assembly failed for request %s, couldn't assemble request: %s", req.FlowIdentifier, err)
+		return file, err
 	}
 
-	finisher := newFinisher(nil)
+	// Finish updating the file state.
+	finisher := newFinisher(s.files)
 	if err := finisher.finish(req, file.ID, req.DirectoryID); err != nil {
-		// log
-		return
+		app.Log.Errorf("Assembly failed for request %s, couldn't finish request: %s", req.FlowIdentifier, err)
+		return file, err
 	}
 
+	app.Log.Infof("successfully upload fileID", file.ID)
+	// Remove upload request.
 	s.tracker.clear(req.UploadID())
+	s.uploads.Delete(req.UploadID())
+	return nil, nil
 }
 
 func (s *uploadService) createFile(req *UploadRequest) (*schema.File, error) {
@@ -83,13 +104,24 @@ func (s *uploadService) createFile(req *UploadRequest) (*schema.File, error) {
 	if err != nil {
 		return nil, err
 	}
-	file := schema.NewFile(req.FlowFileName, req.Owner)
+	file := schema.NewFile(upload.File.Name, req.Owner)
 
 	f, err := s.files.Insert(&file, upload.DirectoryID, upload.ProjectID)
+	app.Log.Infof("Created file %s, in %s %s\n", f.ID, upload.DirectoryID, upload.ProjectID)
 	if err != nil {
 		return nil, err
 	}
 	return f, nil
+}
+
+// createDest creates the destination file and ensures that the directory
+// path is also created.
+func (s *uploadService) createDest(fileID string) (io.Writer, error) {
+	dir := app.MCDir.FileDir(fileID)
+	if err := os.MkdirAll(dir, 0700); err != nil {
+		return nil, err
+	}
+	return os.Create(app.MCDir.FilePath(fileID))
 }
 
 func (s *uploadService) requestDir(req *flow.Request) string {
@@ -100,4 +132,13 @@ func (s *uploadService) requestDir(req *flow.Request) string {
 func (s *uploadService) Write(dest string, req *flow.Request) error {
 	writer := &fileRequestWriter{}
 	return writer.Write(dest, req)
+}
+
+func (s *uploadService) cleanup(req *UploadRequest, fileID string) error {
+	upload, err := s.uploads.ByID(req.FlowIdentifier)
+	if err != nil {
+		return err
+	}
+	_, err = s.files.Delete(fileID, upload.DirectoryID, upload.ProjectID)
+	return err
 }
