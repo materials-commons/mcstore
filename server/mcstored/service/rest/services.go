063697d745368397f4867b874baac8b2e5d2ab3d
V. Glenn Tarcea <gtarcea@umich.edu>
2015-01-30 13:00:25 -0500

    Use the apikey filter.

--- server/mcstored/service/rest/services.go
+++ server/mcstored/service/rest/services.go
@@ -2,7 +2,10 @@ package rest
 
 import (
 	"github.com/emicklei/go-restful"
+	"github.com/materials-commons/mcstore/pkg/db"
+	"github.com/materials-commons/mcstore/pkg/db/dai"
 	"github.com/materials-commons/mcstore/pkg/ws/rest"
+	"github.com/materials-commons/mcstore/server/mcstored/service/rest/filters"
 	"github.com/materials-commons/mcstore/server/mcstored/service/rest/upload"
 	"github.com/materials-commons/mcstore/server/mcstored/service/uploads"
 )
@@ -10,7 +13,9 @@ import (
 // NewServicesContainer creates a new restful.Container made up of all
 // the rest resources handled by the server.
 func NewServicesContainer() *restful.Container {
+	apikeyFilter := filters.NewAPIKeyFilter(dai.NewRUsers(db.RSessionMust()))
 	container := restful.NewContainer()
+	container.Filter(apikeyFilter.Filter)
 	uploadResource := uploadResource()
 	container.Add(uploadResource.WebService())
 	return container
