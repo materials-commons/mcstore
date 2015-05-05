ccee01185ebc75db8e3ca0770a02b9b3041ef860
V. Glenn Tarcea <gtarcea@umich.edu>
2015-05-05 15:43:10 -0400

    Add utility functions for dealing with the api.

--- pkg/app/mcapi.go
+++ pkg/app/mcapi.go
@@ -1,10 +1,16 @@
 package app
 
 import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/url"
 	"strings"
 
+	"github.com/gtarcea/1DevDayTalk2014/app"
 	"github.com/materials-commons/config"
 	"github.com/materials-commons/gohandy/ezhttp"
+	"gnd.la/net/urlutil"
 )
 
 type mcapi struct{}
@@ -21,4 +27,28 @@ func (a mcapi) MCClient() *ezhttp.EzClient {
 		return ezhttp.NewSSLClient()
 	}
 	return ezhttp.NewClient()
+}
+
+func (a mcapi) APIUrl(path string) string {
+	values := url.Values{}
+	values.Add("apikey", config.GetString("apikey"))
+	mcurl := urlutil.MustJoin(a.MCUrl(), path)
+	mcurl = urlutil.AppendQuery(mcurl, values)
+	return mcurl
+}
+
+func (a mcapi) APIError(resp *http.Response, errs []error) error {
+	switch {
+	case len(errs) != 0:
+		return app.ErrInvalid
+	case resp.StatusCode > 299:
+		return fmt.Errorf("HTTP Error: %s", resp.Status)
+	default:
+		return nil
+	}
+}
+
+func (a mcapi) ToJSON(from string, to interface{}) error {
+	err := json.Unmarshal([]byte(from), to)
+	return err
 }
