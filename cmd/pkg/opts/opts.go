78ce8ce9b6b00c229c725f608d86df531628725f
V. Glenn Tarcea <gtarcea@umich.edu>
2015-05-19 14:56:17 -0400

    Move getNumThreads into a separate package so it can be used across multiple commands.

---
+++ cmd/pkg/opts/opts.go
@@ -0,0 +1,25 @@
+package opts
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/codegangsta/cli"
+)
+
+const maxSimultaneous = 5
+
+// getNumThreads ensures that the number of parallel downloads is valid.
+func GetNumThreads(c *cli.Context) int {
+	numThreads := c.Int("parallel")
+
+	if numThreads < 1 {
+		fmt.Println("Simultaneous downloads must be positive: ", numThreads)
+		os.Exit(1)
+	} else if numThreads > maxSimultaneous {
+		fmt.Printf("You may not set simultaneous downloads greater than %d: %d\n", maxSimultaneous, numThreads)
+		os.Exit(1)
+	}
+
+	return numThreads
+}
