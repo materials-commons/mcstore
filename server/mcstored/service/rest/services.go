7b9529d1dd143b7adca4f6b288767ab04ab8a6fe
V. Glenn Tarcea <gtarcea@umich.edu>
2015-05-19 15:31:40 -0400

    Add projects resource instantiation.

--- server/mcstored/service/rest/services.go
+++ server/mcstored/service/rest/services.go
@@ -7,6 +7,7 @@ import (
 	"github.com/materials-commons/mcstore/pkg/ws/rest"
 	"github.com/materials-commons/mcstore/server/mcstored/service/data"
 	"github.com/materials-commons/mcstore/server/mcstored/service/rest/filters"
+	"github.com/materials-commons/mcstore/server/mcstored/service/rest/projects"
 	"github.com/materials-commons/mcstore/server/mcstored/service/rest/upload"
 	"github.com/materials-commons/mcstore/server/mcstored/service/uploads"
 )
@@ -14,15 +15,26 @@ import (
 // NewServicesContainer creates a new restful.Container made up of all
 // the rest resources handled by the server.
 func NewServicesContainer() *restful.Container {
-	apikeyFilter := filters.NewAPIKeyFilter(dai.NewRUsers(db.RSessionMust()))
 	container := restful.NewContainer()
+
+	apikeyFilter := filters.NewAPIKeyFilter(dai.NewRUsers(db.RSessionMust()))
 	container.Filter(apikeyFilter.Filter)
+
 	uploadResource := uploadResource()
 	container.Add(uploadResource.WebService())
+
+	projectsResource := projectsResource()
+	container.Add(projectsResource.WebService())
+
 	return container
 }
 
 // uploadResource creates a new upload resource.
 func uploadResource() rest.Service {
 	return upload.NewResource(uploads.NewUploadService(), uploads.NewIDService(), data.NewDirService())
+}
+
+// projectsResource creates a new projects resource.
+func projectsResource() rest.Service {
+	return projects.NewResource(data.NewDirService())
 }
