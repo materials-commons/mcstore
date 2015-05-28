e712923c291f28d3df5139801dc9f570d42b5d2a
V. Glenn Tarcea <gtarcea@umich.edu>
2015-05-28 14:02:30 -0400

    Update test description.

--- server/mcstore/datahandler_test.go
+++ server/mcstore/datahandler_test.go
@@ -94,7 +94,7 @@ var _ = Describe("DataHandler", func() {
 			Expect(path).To(Equal(app.MCDir.FilePathImageConversion(f.FileID())))
 		})
 
-		It("Should return original with converted image when asking for original", func() {
+		It("Should return original even though converted is available when asking for original", func() {
 			f.MediaType.Mime = "image/tiff"
 			path := filePath(&f, true)
 			Expect(path).To(Equal(app.MCDir.FilePath(f.FileID())))
