103478193b1c5286c0077e749fe74a398a729933
V. Glenn Tarcea <gtarcea@umich.edu>
2015-05-29 09:52:54 -0400

    Add mock that works with inserts. This doesn't use the stretchr mock package.

--- pkg/db/dai/mocks/Dirs.go
+++ pkg/db/dai/mocks/Dirs.go
@@ -2,7 +2,10 @@ package mocks
 
 import "github.com/materials-commons/testify/mock"
 
-import "github.com/materials-commons/mcstore/pkg/db/schema"
+import (
+	"github.com/materials-commons/mcstore/pkg/db/schema"
+	"fmt"
+)
 
 type Dirs struct {
 	mock.Mock
@@ -42,40 +45,67 @@ func (m *Dirs) Insert(dir *schema.Directory) (*schema.Directory, error) {
 	return r0, r1
 }
 
-type Dirs2 struct {
+type entry struct {
 	dir   *schema.Directory
 	err   error
 	files []schema.File
 }
 
+type Dirs2 struct {
+	method map[string]*entry
+	currentMethod string
+}
+
 func NewMDirs2() *Dirs2 {
-	return &Dirs2{}
+	return &Dirs2{
+		method: make(map[string]*entry),
+	}
+}
+
+func (m *Dirs2) lookup(method string) *entry {
+	if e, ok := m.method[method]; ok {
+		return e
+	}
+	panic(fmt.Sprintf("Unable to find method: %s", method))
 }
 
 func (m *Dirs2) ByID(id string) (*schema.Directory, error) {
-	return m.dir, m.err
+	e := m.lookup("ByID")
+	return e.dir, e.err
 }
 
 func (m *Dirs2) ByPath(path, projectID string) (*schema.Directory, error) {
-	return m.dir, m.err
+	e := m.lookup("ByPath")
+	return e.dir, e.err
 }
 
 func (m *Dirs2) Files(dirID string) ([]schema.File, error) {
-	return m.files, m.err
+	e := m.lookup("Files")
+	return e.files, e.err
 }
 
 func (m *Dirs2) Insert(dir *schema.Directory) (*schema.Directory, error) {
-	return m.dir, m.err
+	e := m.lookup("Insert")
+	return e.dir, e.err
 }
 
-func (m *Dirs2) SetError(err error) {
-	m.err = err
+func(m *Dirs2) On(method string) *Dirs2 {
+	m.currentMethod = method
+	m.method[method] = &entry{}
+	return m
 }
 
-func (m *Dirs2) SetDir(dir *schema.Directory) {
-	m.dir = dir
+func (m *Dirs2) SetError(err error) *Dirs2 {
+	m.method[m.currentMethod].err = err
+	return m
 }
 
-func (m *Dirs2) SetFiles(files []schema.File) {
-	m.files = files
+func (m *Dirs2) SetDir(dir *schema.Directory) *Dirs2 {
+	m.method[m.currentMethod].dir = dir
+	return m
+}
+
+func (m *Dirs2) SetFiles(files []schema.File) *Dirs2 {
+	m.method[m.currentMethod].files = files
+	return m
 }
