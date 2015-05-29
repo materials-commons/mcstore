42f64f973e8a19930f8b2d835ed748509ebafd82
V. Glenn Tarcea <gtarcea@umich.edu>
2015-05-29 10:37:13 -0400

    Add second type of mock.

--- pkg/db/dai/mocks/Projects.go
+++ pkg/db/dai/mocks/Projects.go
@@ -1,6 +1,10 @@
 package mocks
 
-import "github.com/materials-commons/testify/mock"
+import (
+	"fmt"
+
+	"github.com/materials-commons/testify/mock"
+)
 
 import "github.com/materials-commons/mcstore/pkg/db/schema"
 
@@ -48,4 +52,80 @@ func (m *Projects) AccessList(projectID string) ([]schema.Access, error) {
 	r0 := ret.Get(0).([]schema.Access)
 	r1 := ret.Error(1)
 	return r0, r1
+}
+
+type pentry struct {
+	project *schema.Project
+	hasDir  bool
+	err     error
+	access  []schema.Access
+}
+
+type Projects2 struct {
+	method        map[string]*pentry
+	currentMethod string
+}
+
+func NewMProjects2() *Projects2 {
+	return &Projects2{
+		method: make(map[string]*pentry),
+	}
+}
+
+func (m *Projects2) lookup(method string) *pentry {
+	if e, ok := m.method[method]; ok {
+		return e
+	}
+	panic(fmt.Sprintf("Unable to find method: %s", method))
+}
+
+func (m *Projects2) ByID(id string) (*schema.Project, error) {
+	e := m.lookup("ByID")
+	return e.project, e.err
+}
+
+func (m *Projects2) ByName(name, owner string) (*schema.Project, error) {
+	e := m.lookup("ByName")
+	return e.project, e.err
+}
+
+func (m *Projects2) Insert(project *schema.Project) (*schema.Project, error) {
+	e := m.lookup("Insert")
+	return e.project, e.err
+}
+
+func (m *Projects2) HasDirectory(projectID, directoryID string) bool {
+	e := m.lookup("HasDirectory")
+	return e.hasDir
+}
+
+func (m *Projects2) AccessList(projectID string) ([]schema.Access, error) {
+	e := m.lookup("AccessList")
+	return e.access, e.err
+}
+
+func (m *Projects2) On(method string) *Projects2 {
+	m.currentMethod = method
+	m.method[method] = &pentry{}
+	return m
+}
+
+func (m *Projects2) SetError(err error) *Projects2 {
+	m.method[m.currentMethod].err = err
+	return m
+}
+
+func (m *Projects2) SetProject(project *schema.Project) *Projects2 {
+	m.method[m.currentMethod].project = project
+	return m
+}
+
+func (m *Projects2) SetHasDir(hasDir bool) *Projects2 {
+	m.method[m.currentMethod].hasDir = hasDir
+	return m
+}
+
+func (m *Projects2) SetAccessList(access []schema.Access) *Projects2 {
+	m.method[m.currentMethod].access = access
+	return m
 }
