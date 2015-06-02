1360fc7e311fc799ca5d41da069eb4097505f201
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-02 15:54:54 -0400

    Skeleton for unit tests for requestWriter.

---
+++ server/mcstore/uploads/request_writer_test.go
@@ -0,0 +1,24 @@
+package uploads
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("requestWriter", func() {
+	Describe("blockRequestWriter", func() {
+		Context("writeRequest method tests", func() {
+
+		})
+
+		Context("write method tests", func() {
+			It("Should fail", func() {
+				Expect("").To(Equal(""))
+			})
+		})
+
+		Context("validate method tests", func() {
+
+		})
+	})
+})
