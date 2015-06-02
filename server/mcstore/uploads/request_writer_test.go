fc4b5ad4851ae4c48f936bf38da2cbec14ce26f0
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-02 17:54:49 -0400

    Add test for single block write.

--- server/mcstore/uploads/request_writer_test.go
+++ server/mcstore/uploads/request_writer_test.go
@@ -1,13 +1,46 @@
 package uploads
 
 import (
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"github.com/materials-commons/mcstore/pkg/app/flow"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("requestWriter", func() {
 	Describe("blockRequestWriter", func() {
-		Context("writeRequest method tests", func() {
+		var (
+			brWriter *blockRequestWriter = &blockRequestWriter{}
+			dirPath  string              = filepath.Join(os.TempDir(), "rr")
+			filePath string              = filepath.Join(dirPath, "req")
+			req      *flow.Request       = &flow.Request{
+				FlowIdentifier:  "req",
+				FlowChunkNumber: 1,
+			}
+		)
+
+		BeforeEach(func() {
+			os.MkdirAll(dirPath, 0770)
+		})
+
+		AfterEach(func() {
+			//os.RemoveAll(dirPath)
+		})
+
+		Context("write method tests", func() {
+			It("Should correctly write a single block file", func() {
+				data := []byte("hello")
+				req.Chunk = data
+				req.FlowTotalSize = int64(len(data))
+				err := brWriter.write(dirPath, req)
+				Expect(err).To(BeNil(), "Error = %s", err)
+				content, err := ioutil.ReadFile(filePath)
+				Expect(err).To(BeNil())
+				Expect(content).To(Equal(data))
+			})
 
 		})
 
