309d081653efe71125182a7f348b4ad7fc887729
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-03 14:36:43 -0400

    Skeleton for upload resource testing.

---
+++ server/mcstore/upload_resource_test.go
@@ -0,0 +1,12 @@
+package mcstore
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("UploadResource", func() {
+	It("Should do something", func() {
+		Expect("").To(Equal(""))
+	})
+})
