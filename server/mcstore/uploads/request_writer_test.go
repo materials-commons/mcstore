5244bd855a230215a6f99f5f2f5a33428127e6cc
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-02 20:58:39 -0400

    Add tests for multi block writes.

--- server/mcstore/uploads/request_writer_test.go
+++ server/mcstore/uploads/request_writer_test.go
@@ -28,13 +28,14 @@ var _ = Describe("requestWriter", func() {
 		})
 
 		AfterEach(func() {
-			//os.RemoveAll(dirPath)
+			os.RemoveAll(dirPath)
 		})
 
 		Context("write method tests", func() {
 			It("Should correctly write a single block file", func() {
 				data := []byte("hello")
 				req.Chunk = data
+				req.FlowChunkNumber = int32(len(data))
 				req.FlowTotalSize = int64(len(data))
 				err := brWriter.write(dirPath, req)
 				Expect(err).To(BeNil(), "Error = %s", err)
@@ -43,6 +44,57 @@ var _ = Describe("requestWriter", func() {
 				Expect(content).To(Equal(data))
 			})
 
+			It("Should correctly write a multiple block file", func() {
+				data1 := []byte("my")
+				data2 := []byte("da")
+				req.Chunk = data1
+				req.FlowChunkSize = 2
+				req.FlowTotalSize = int64(len(data1) + len(data2))
+				err := brWriter.write(dirPath, req)
+				Expect(err).To(BeNil(), "error = %s", err)
+				content, err := ioutil.ReadFile(filePath)
+				Expect(err).To(BeNil())
+
+				// The file has a length of 3, since we only wrote 2 and the other parts haven't
+				// yet been written, we just compare the FlowChunkSize (2) bytes.
+				Expect(content[:req.FlowChunkSize]).To(Equal(data1), "not equal '%s'/'%s'", string(content), string(data1))
+				req.Chunk = data2
+				req.FlowChunkNumber = 2
+				err = brWriter.write(dirPath, req)
+				Expect(err).To(BeNil(), "error = %s", err)
+				content, err = ioutil.ReadFile(filePath)
+				Expect(err).To(BeNil())
+				var dataCombined []byte
+				dataCombined = append(dataCombined, data1...)
+				dataCombined = append(dataCombined, data2...)
+				Expect(content).To(Equal(dataCombined))
+			})
+		})
+
+		It("Should correctly write a multiple block file with the final block short", func() {
+			data1 := []byte("my")
+			data2 := []byte("d")
+			req.Chunk = data1
+			req.FlowChunkSize = 2
+			req.FlowTotalSize = int64(len(data1) + len(data2))
+			err := brWriter.write(dirPath, req)
+			Expect(err).To(BeNil(), "error = %s", err)
+			content, err := ioutil.ReadFile(filePath)
+			Expect(err).To(BeNil())
+
+			// The file has a length of 3, since we only wrote 2 and the other parts haven't
+			// yet been written, we just compare the FlowChunkSize (2) bytes.
+			Expect(content[:req.FlowChunkSize]).To(Equal(data1), "not equal '%s'/'%s'", string(content), string(data1))
+			req.Chunk = data2
+			req.FlowChunkNumber = 2
+			err = brWriter.write(dirPath, req)
+			Expect(err).To(BeNil(), "error = %s", err)
+			content, err = ioutil.ReadFile(filePath)
+			Expect(err).To(BeNil())
+			var dataCombined []byte
+			dataCombined = append(dataCombined, data1...)
+			dataCombined = append(dataCombined, data2...)
+			Expect(content).To(Equal(dataCombined))
 		})
 
 		Context("write method tests", func() {
