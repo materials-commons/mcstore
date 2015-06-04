8a659113302b6ed857c5fe84af643e5fcb515e7d
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-04 15:22:02 -0400

    Add Files2 mock interface

--- pkg/db/dai/mocks/Files.go
+++ pkg/db/dai/mocks/Files.go
@@ -2,7 +2,10 @@ package mocks
 
 import "github.com/materials-commons/testify/mock"
 
-import "github.com/materials-commons/mcstore/pkg/db/schema"
+import (
+	"github.com/materials-commons/mcstore/pkg/db/schema"
+	"fmt"
+)
 
 type Files struct {
 	mock.Mock
@@ -68,4 +71,89 @@ func (m *Files) GetProject(fileID string) (*schema.Project, error) {
 	r0 := ret.Get(0).(*schema.Project)
 	r1 := ret.Error(1)
 	return r0, r1
+}
+
+type fentry struct {
+	file *schema.File
+	err error
+	project *schema.Project
+}
+
+type Files2 struct {
+	method map[string]*fentry
+	currentMethod string
+}
+
+func NewMFiles2() *Files2 {
+	return &Files2{
+		method: make(map[string]*fentry),
+	}
+}
+
+func (m *Files2) lookup(method string) *fentry {
+	if e, ok := m.method[method]; ok {
+		return e
+	}
+	panic(fmt.Sprintf("Unable to find method: %s", method))
+}
+
+func (m *Files2) ByID(id string) (*schema.File, error) {
+	e := m.lookup("ByID")
+	return e.file, e.err
+}
+
+func (m *Files2) ByChecksum(checksum string) (*schema.File, error) {
+	e := m.lookup("ByChecksum")
+	return e.file, e.err
+}
+
+func (m *Files2) ByPath(name, dirID string) (*schema.File, error) {
+	e := m.lookup("ByPath")
+	return e.file, e.err
+}
+
+func (m *Files2) Insert(file *schema.File, dirID string, projectID string) (*schema.File, error) {
+	e := m.lookup("Insert")
+	return e.file, e.err
+}
+
+func (m *Files2) Update(file *schema.File) error {
+	e := m.lookup("Update")
+	return e.err
+}
+
+func (m *Files2) UpdateFields(fileID string, fields map[string]interface{}) error {
+	e := m.lookup("UpdateFields")
+	return e.err
+}
+
+func (m *Files2) Delete(fileID, directoryID, projectID string) (*schema.File, error) {
+	e := m.lookup("Delete")
+	return e.file, e.err
+}
+
+func (m *Files2) GetProject(fileID string) (*schema.Project, error) {
+	e := m.lookup("GetProject")
+	return e.project, e.err
+}
+
+func (m *Files2) On(method string) *Files2 {
+	m.currentMethod = method
+	m.method[method] = &fentry{}
+	return m
+}
+
+func (m *Files2) SetError(err error) *Files2 {
+	m.method[m.currentMethod].err = err
+	return m
+}
+
+func (m *Files2) SetFile(file *schema.File) *Files2 {
+	m.method[m.currentMethod].file = file
+	return m
+}
+
+func (m *Files2) SetProject(project *schema.Project) *Files2 {
+	m.method[m.currentMethod].project = project
+	return m
 }
