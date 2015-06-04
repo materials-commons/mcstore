eb4a4fcff712b78b4aec920a62f1e0ea8cc3c0e6
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-04 14:14:39 -0400

    Add skeleton unit tests for upload service.

--- server/mcstore/uploads/upload_service_test.go
+++ server/mcstore/uploads/upload_service_test.go
@@ -1,26 +1,75 @@
 package uploads
 
 import (
+	dmocks "github.com/materials-commons/mcstore/pkg/db/dai/mocks"
+
+	"github.com/materials-commons/config"
+	"github.com/materials-commons/gohandy/file"
+	"github.com/materials-commons/mcstore/pkg/app"
+	"github.com/materials-commons/mcstore/pkg/app/flow"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
 var _ = Describe("UploadService", func() {
 	var (
-		s UploadService
+		s              *uploadService
+		mdirs          *dmocks.Dirs
+		muploads       *dmocks.Uploads
+		mfiles         *dmocks.Files
+		req            *UploadRequest
+		f              *flow.Request
+		savedMCDIRPath string
 	)
 
 	BeforeEach(func() {
-		s = NewUploadService()
+		savedMCDIRPath = app.MCDir.Path()
+		config.Set("MCDIR", "/tmp")
+		mdirs = dmocks.NewMDirs()
+		muploads = dmocks.NewMUploads()
+		mfiles = dmocks.NewMFiles()
+		s = &uploadService{
+			files:       mfiles,
+			dirs:        mdirs,
+			uploads:     muploads,
+			tracker:     requestBlockTracker,
+			writer:      &blockRequestWriter{},
+			requestPath: &mcdirRequestPath{},
+			fops:        file.OS,
+		}
+
+		f = &flow.Request{
+			FlowChunkSize:   1024,
+			FlowTotalSize:   int64(len("hello")),
+			FlowChunkNumber: 1,
+			Chunk:           []byte("hello"),
+			FlowIdentifier:  "req",
+		}
+
+		req = &UploadRequest{f}
 	})
 
-	Describe("something", func() {
-		Context("context", func() {
-			It("Should do something", func() {
+	AfterEach(func() {
+		config.Set("MCDIR", savedMCDIRPath)
+	})
+
+	Describe("Upload method tests", func() {
+		Context("Successful upload cases", func() {
+			It("Should succeed for a single block upload", func() {
 				str := ""
 				Expect(str).To(Equal(""))
 			})
 		})
+
+		Context("Failed upload cases", func() {
+			It("Should succeed for a multiple block upload", func() {
+
+			})
+		})
+	})
+
+	Describe("assemble method tests", func() {
+
 	})
 
 })
