f5ce4d92b3e293b73624ce4ac0f0ca14b4260b00
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-05 13:26:32 -0400

    Switch tests to ginkgo.

--- pkg/db/dai/rfiles_test.go
+++ pkg/db/dai/rfiles_test.go
@@ -1,80 +1,94 @@
 package dai
 
 import (
-	"testing"
+	"fmt"
 
 	r "github.com/dancannon/gorethink"
 	"github.com/materials-commons/mcstore/pkg/app"
 	"github.com/materials-commons/mcstore/pkg/db/model"
 	"github.com/materials-commons/mcstore/pkg/db/schema"
 	"github.com/materials-commons/mcstore/testutil"
-	"github.com/stretchr/testify/require"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
 )
 
-var rfiles = NewRFiles(testutil.RSession())
+var _ = Describe("RFiles", func() {
+	var rfiles Files
 
-func TestRFilesByID(t *testing.T) {
-	// Test existing
-	f, err := rfiles.ByID("testfile.txt")
-	require.Nil(t, err, "Unable retrieve existing file: %s", err)
-	require.NotNil(t, f, "Found file, but returned nil for entry")
-	require.Equal(t, f.ID, "testfile.txt", "Retrieved wrong file %#v", f)
+	BeforeEach(func() {
+		rfiles = NewRFiles(testutil.RSession())
+	})
 
-	// Test no such file id
-	f, err = rfiles.ByID("does-not-exist")
-	require.Equal(t, err, app.ErrNotFound, "Found file that doesn't exist")
-	require.Nil(t, f, "Returned file entry rather than nil %#v", f)
-}
+	Describe("ByID", func() {
+		It("Should find existing file", func() {
+			f, err := rfiles.ByID("testfile.txt")
+			Expect(err).To(BeNil())
+			Expect(f).NotTo(BeNil())
+			Expect(f.ID).To(Equal("testfile.txt"))
+		})
 
-func TestRFilesInsertIDSet(t *testing.T) {
-	file := schema.NewFile("test1.txt", "test@mc.org")
-	file.ID = "test1.txt" // Explicitly set the ID
+		It("Should return an error when file doesn't exist", func() {
+			f, err := rfiles.ByID("does-not-exist")
+			Expect(err).To(Equal(app.ErrNotFound))
+			Expect(f).To(BeNil())
+		})
+	})
 
-	newFile, err := rfiles.Insert(&file, "test", "test")
-	require.Nil(t, err)
-	require.NotNil(t, newFile)
-	require.Equal(t, newFile.ID, "test1.txt")
+	Describe("Insert", func() {
+		Context("Explicitly set ID", func() {
+			It("Should properly create the file plus all the join table entries", func() {
+				file := schema.NewFile("test1.txt", "test@mc.org")
+				file.ID = "test1.txt" // Explicitly set the ID
+				newFile, err := rfiles.Insert(&file, "test", "test")
+				Expect(err).To(BeNil())
+				Expect(newFile.ID).To(Equal("test1.txt"))
 
-	// Check that the join tables were updated.
-	session := testutil.RSession()
-	var p2df []schema.Project2DataFile
-	rql := r.Table("project2datafile").Filter(r.Row.Field("datafile_id").Eq(file.ID))
-	err = model.ProjectFiles.Qs(session).Rows(rql, &p2df)
-	require.Nil(t, err)
-	require.Equal(t, len(p2df), 1)
+				// Check that the join tables were updated.
+				session := testutil.RSession()
+				var p2df []schema.Project2DataFile
+				rql := r.Table("project2datafile").Filter(r.Row.Field("datafile_id").Eq(file.ID))
+				err = model.ProjectFiles.Qs(session).Rows(rql, &p2df)
+				Expect(err).To(BeNil())
+				Expect(len(p2df)).To(BeNumerically("==", 1))
 
-	var dir2df []schema.DataDir2DataFile
-	rql = r.Table("datadir2datafile").Filter(r.Row.Field("datafile_id").Eq(file.ID))
-	err = model.DirFiles.Qs(session).Rows(rql, &dir2df)
-	require.Nil(t, err)
-	require.Equal(t, len(dir2df), 1)
+				var dir2df []schema.DataDir2DataFile
+				rql = r.Table("datadir2datafile").Filter(r.Row.Field("datafile_id").Eq(file.ID))
+				err = model.DirFiles.Qs(session).Rows(rql, &dir2df)
+				Expect(err).To(BeNil())
+				Expect(len(dir2df)).To(BeNumerically("==", 1))
 
-	deleteFile(file.ID)
-}
+				deleteFile(file.ID)
+			})
+		})
 
-func TestRFilesInsertNoIDSet(t *testing.T) {
-	file := schema.NewFile("test1.txt", "test@mc.org")
+		Context("Database sets ID", func() {
+			It("Should properly create the file plus all the join table entries", func() {
+				file := schema.NewFile("test1.txt", "test@mc.org")
 
-	newFile, err := rfiles.Insert(&file, "test", "test")
-	require.Nil(t, err)
-	require.NotNil(t, newFile)
+				fmt.Printf("%#v", rfiles)
+				newFile, err := rfiles.Insert(&file, "test", "test")
+				Expect(err).To(BeNil())
+				Expect(newFile.ID).NotTo(Equal(""))
 
-	// Check that the join tables were updated.
-	session := testutil.RSession()
-	var p2df []schema.Project2DataFile
-	rql := r.Table("project2datafile").Filter(r.Row.Field("datafile_id").Eq(newFile.ID))
-	err = model.ProjectFiles.Qs(session).Rows(rql, &p2df)
-	require.Nil(t, err)
-	require.Equal(t, len(p2df), 1)
+				// Check that the join tables were updated.
+				session := testutil.RSession()
+				var p2df []schema.Project2DataFile
+				rql := r.Table("project2datafile").Filter(r.Row.Field("datafile_id").Eq(newFile.ID))
+				err = model.ProjectFiles.Qs(session).Rows(rql, &p2df)
+				Expect(err).To(BeNil())
+				Expect(len(p2df)).To(BeNumerically("==", 1))
 
-	var dir2df []schema.DataDir2DataFile
-	rql = r.Table("datadir2datafile").Filter(r.Row.Field("datafile_id").Eq(newFile.ID))
-	err = model.DirFiles.Qs(session).Rows(rql, &dir2df)
-	require.Nil(t, err)
-	require.Equal(t, len(dir2df), 1)
+				var dir2df []schema.DataDir2DataFile
+				rql = r.Table("datadir2datafile").Filter(r.Row.Field("datafile_id").Eq(newFile.ID))
+				err = model.DirFiles.Qs(session).Rows(rql, &dir2df)
+				Expect(err).To(BeNil())
+				Expect(len(dir2df)).To(BeNumerically("==", 1))
 
-	deleteFile(newFile.ID)
-}
+				deleteFile(newFile.ID)
+			})
+		})
+	})
+})
 
 func deleteFile(fileID string) {
 	session := testutil.RSession()
