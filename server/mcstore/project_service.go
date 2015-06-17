366a0162fcb28d3d515a0aa1d0ddc82fa88caf23
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-17 14:40:39 -0400

    Add Access to project service. Begin filling out extensions to get project/projects.

--- server/mcstore/project_service.go
+++ server/mcstore/project_service.go
@@ -6,27 +6,33 @@ import (
 	"github.com/materials-commons/mcstore/pkg/db"
 	"github.com/materials-commons/mcstore/pkg/db/dai"
 	"github.com/materials-commons/mcstore/pkg/db/schema"
+	"github.com/materials-commons/mcstore/pkg/domain"
 )
 
 // ProjectService is a service for manipulating projects.
 type ProjectService interface {
 	createProject(projectName, owner string, mustNotExist bool) (*schema.Project, bool, error)
-	getProject(projectName, owner string, create bool) (*schema.Project, bool, error)
+	getProjectByName(projectName, owner, user string) (*schema.Project, error)
+	getProjectByID(projectID, user string) (*schema.Project, error)
 }
 
 // projectService implements the ProjectService interface
 type projectService struct {
 	projects dai.Projects
 	dirs     dai.Dirs
+	access   domain.Access
 }
 
 // newProjectService creates a new instance of the ProjectService. NewProjectService will panic
 // if it cannot attach to the database.
 func newProjectService() *projectService {
 	session := db.RSessionMust()
+	access := domain.NewAccess(dai.NewRProjects(session), dai.NewRFiles(session), dai.NewRUsers(session))
+
 	return &projectService{
 		projects: dai.NewRProjects(session),
 		dirs:     dai.NewRDirs(session),
+		access:   access,
 	}
 }
 
@@ -62,30 +68,36 @@ func (s *projectService) createProject(projectName, owner string, mustNotExist b
 	}
 }
 
-// getProject attempts to get a project by project name for the given user. If the create flag is
-// set then it will create the project if it doesn't exist. If GetProject creates a project it
-// will return true, otherwise it will return false.
-func (s *projectService) getProject(projectName, owner string, create bool) (*schema.Project, bool, error) {
-	project, err := s.projects.ByName(projectName, owner)
+// createNewProject will create a new project.
+func (s *projectService) createNewProject(projectName, owner string) (*schema.Project, error) {
+	project := schema.NewProject(projectName, owner)
+	return s.projects.Insert(&project)
+}
 
+// getProjectByName returns the project matching name owned by user. It validates
+// access to the project and returns app.ErrNoAccess if user doesn't have access.
+func (s *projectService) getProjectByName(projectName, owner, user string) (*schema.Project, error) {
+	proj, err := s.projects.ByName(projectName, owner)
 	switch {
-	case err == app.ErrNotFound:
-		if create {
-			proj, err := s.createNewProject(projectName, owner)
-			return proj, true, err
-		}
-		return nil, false, app.ErrNotFound
-
 	case err != nil:
-		return nil, false, err
-
+		return nil, err
+	case !s.access.AllowedByOwner(proj.ID, user):
+		return nil, app.ErrNoAccess
 	default:
-		return project, false, nil
+		return proj, nil
 	}
 }
 
-// createNewProject will create a new project.
-func (s *projectService) createNewProject(projectName, owner string) (*schema.Project, error) {
-	project := schema.NewProject(projectName, "", owner)
-	return s.projects.Insert(&project)
+// getProjectByID returns the project matching id. It validates access to the project
+// and returns app.ErrNoAccess if user doesn't have access.
+func (s *projectService) getProjectByID(projectID, user string) (*schema.Project, error) {
+	proj, err := s.projects.ByID(projectID)
+	switch {
+	case err != nil:
+		return nil, err
+	case !s.access.AllowedByOwner(proj.ID, user):
+		return nil, app.ErrNoAccess
+	default:
+		return proj, nil
+	}
 }
