3a1143d2a0000ee997e55980327371221a0b4247
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-18 15:13:24 -0400

    Create skeleton for testing projectUploader.

---
+++ cmd/pkg/mc/project_uploader_test.go
@@ -0,0 +1,46 @@
+package mc
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("ProjectUploader", func() {
+	Describe("createDirectory", func() {
+
+	})
+
+	Describe("handleDirEntry", func() {
+
+	})
+
+	Describe("sendFlowReq", func() {
+
+	})
+
+	Describe("getUploadResponse", func() {
+
+	})
+
+	Describe("uploadFile", func() {
+
+	})
+
+	Describe("getDirByPath", func() {
+
+	})
+
+	Describe("getFileByName", func() {
+
+	})
+
+	Describe("handleFileEntry", func() {
+
+	})
+
+	Describe("uploadEntry", func() {
+		It("Should", func() {
+			Expect("").To(Equal(""))
+		})
+	})
+})
