ccd7e4be84a6eed889043b602e0fbecae4dd2c21
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-21 09:47:25 -0400

    Remove extraneous return.

--- server/mcstore/database_session_filter.go
+++ server/mcstore/database_session_filter.go
@@ -14,7 +14,6 @@ type databaseSessionFilter struct {
 func (f *databaseSessionFilter) Filter(req *restful.Request, resp *restful.Response, chain *restful.FilterChain) {
 	if session, err := f.session(); err != nil {
 		resp.WriteErrorString(http.StatusInternalServerError, "Unable to connect to database")
-		return
 	} else {
 		req.SetAttribute("session", session)
 		chain.ProcessFilter(req, resp)
