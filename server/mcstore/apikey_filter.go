2544508fbeda49d7053b6f4849790d65fba7f305
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-25 14:25:32 -0400

    Remove commented out code showing reading from the Changes api. Refactor code to make it a little bit clearer.

--- server/mcstore/apikey_filter.go
+++ server/mcstore/apikey_filter.go
@@ -22,61 +22,41 @@ func newAPIKeyFilter(keycache *apikeyCache) *apikeyFilter {
 	}
 }
 
-// changes will monitor for changes to user apikeys and will
-// update the server with the new key.
-//func (f *apikeyFilter) changes() {
-//	var session *r.Session
-//	go func() {
-//		var c struct {
-//			NewUserValue schema.User `gorethink:"new_value"`
-//			OldUserValue schema.User `gorethink:"old_value"`
-//		}
-//		users, _ := r.Table("users").Changes().Run(session)
-//		for users.Next(&c) {
-//			switch {
-//			case c.OldUserValue.ID == "":
-//				// no old id, so new user added
-//				f.keycache.addKey(c.NewUserValue.APIKey, &c.NewUserValue)
-//			case c.OldUserValue.APIKey != "" && c.OldUserValue.APIKey != c.NewUserValue.APIKey:
-//				f.keycache.resetKey(c.OldUserValue.APIKey, c.NewUserValue.APIKey, &c.NewUserValue)
-//			}
-//		}
-//	}()
-//}
-
 // Filter implements the Filter interface for apikey lookup. It checks if an apikey is
 // valid. If the apikey is found it sets the "user" attribute to the user structure. If
 // the apikey is invalid then the filter doesn't pass the request on, and instead returns
 // an http.StatusUnauthorized.
 func (f *apikeyFilter) Filter(request *restful.Request, response *restful.Response, chain *restful.FilterChain) {
-	apikey := request.Request.URL.Query().Get("apikey")
-	session := request.Attribute("session").(*r.Session)
-	rusers := dai.NewRUsers(session)
-	user, found := f.getUser(apikey, rusers)
-	if !found {
+	if apikey := request.Request.URL.Query().Get("apikey"); apikey == "" {
+		// No or blank apikey passed in
 		response.WriteErrorString(http.StatusUnauthorized, "Not authorized")
 	} else {
-		request.SetAttribute("user", *user)
-		chain.ProcessFilter(request, response)
+		session := request.Attribute("session").(*r.Session)
+		rusers := dai.NewRUsers(session)
+		if user := f.getUser(apikey, rusers); user == nil {
+			response.WriteErrorString(http.StatusUnauthorized, "Not authorized")
+		} else {
+			request.SetAttribute("user", *user)
+			chain.ProcessFilter(request, response)
+		}
 	}
 }
 
 // getUser matches the user with the apikey. If it cannot find a match then it returns false.
-// getUser caches the key/user pair in f.apikeys.
-func (f *apikeyFilter) getUser(apikey string, users dai.Users) (*schema.User, bool) {
-	if apikey == "" {
-		// No key was passed.
-		return nil, false
+func (f *apikeyFilter) getUser(apikey string, users dai.Users) *schema.User {
+	if user := f.keycache.getUser(apikey); user != nil {
+		return user
 	}
+	return f.loadUserFromDB(apikey, users)
+}
 
-	if user := f.keycache.getUser(apikey); user == nil {
-		user, err := users.ByAPIKey(apikey)
-		if err != nil {
-			return nil, false
-		}
-		f.keycache.addKey(apikey, user)
-		return user, true
+// loadUserFromDB will look up the user by the apikey. If found it will add the
+// user to cache and return the user. Otherwise it will return nil.
+func (f *apikeyFilter) loadUserFromDB(apikey string, users dai.Users) *schema.User {
+	if user, err := users.ByAPIKey(apikey); err != nil {
+		return nil
 	} else {
-		return user, true
+		f.keycache.addKey(apikey, user)
+		return user
 	}
 }
