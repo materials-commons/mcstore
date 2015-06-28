1821d6c2e1255ec13781a84523a1598287ad8012
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-28 12:52:56 -0400

    Change s to service.

--- server/mcstore/uploads/upload_service.go
+++ server/mcstore/uploads/upload_service.go
@@ -61,27 +61,27 @@ func NewUploadService(session *r.Session) *uploadService {
 
 // Upload performs uploading a block and constructing the file
 // after all blocks have been uploaded.
-func (s *uploadService) Upload(req *UploadRequest) (*UploadStatus, error) {
-	dir := s.requestPath.dir(req.Request)
+func (service *uploadService) Upload(req *UploadRequest) (*UploadStatus, error) {
+	dir := service.requestPath.dir(req.Request)
 	id := req.UploadID()
 
-	if !s.tracker.idExists(id) {
+	if !service.tracker.idExists(id) {
 		return nil, app.ErrInvalid
 	}
 
-	if err := s.writeBlock(dir, req); err != nil {
+	if err := service.writeBlock(dir, req); err != nil {
 		app.Log.Errorf("Writing block %d for request %s failed: %s", req.FlowChunkNumber, id, err)
 		return nil, err
 	}
 
 	uploadStatus := &UploadStatus{}
 
-	if s.tracker.done(id) {
-		if file, err := s.assemble(req, dir); err != nil {
+	if service.tracker.done(id) {
+		if file, err := service.assemble(req, dir); err != nil {
 			app.Log.Errorf("Assembly failed for request %s: %s", req.FlowIdentifier, err)
 			// Assembly failed. If file isn't nil then we need to cleanup state.
 			if file != nil {
-				if err := s.cleanup(req, file.ID); err != nil {
+				if err := service.cleanup(req, file.ID); err != nil {
 					app.Log.Errorf("Attempted cleanup of failed assembly %s errored with: %s", req.FlowIdentifier, err)
 				}
 			}
@@ -97,29 +97,29 @@ func (s *uploadService) Upload(req *UploadRequest) (*UploadStatus, error) {
 
 // writeBlock will write the request block and update state information
 // on the block only if this block hasn't already been written.
-func (s *uploadService) writeBlock(dir string, req *UploadRequest) error {
+func (service *uploadService) writeBlock(dir string, req *UploadRequest) error {
 	id := req.UploadID()
-	if !s.tracker.isBlockSet(id, int(req.FlowChunkNumber)) {
-		if err := s.writer.write(dir, req.Request); err != nil {
+	if !service.tracker.isBlockSet(id, int(req.FlowChunkNumber)) {
+		if err := service.writer.write(dir, req.Request); err != nil {
 			return err
 		}
-		s.tracker.addToHash(id, req.Chunk)
-		s.tracker.setBlock(id, int(req.FlowChunkNumber))
+		service.tracker.addToHash(id, req.Chunk)
+		service.tracker.setBlock(id, int(req.FlowChunkNumber))
 	}
 	return nil
 }
 
 // assemble moves the upload file to its proper location, creates a database entry
 // and take care of all book keeping tasks to make the file accessible.
-func (s *uploadService) assemble(req *UploadRequest, dir string) (*schema.File, error) {
+func (service *uploadService) assemble(req *UploadRequest, dir string) (*schema.File, error) {
 	// Look up the upload
-	upload, err := s.uploads.ByID(req.FlowIdentifier)
+	upload, err := service.uploads.ByID(req.FlowIdentifier)
 	if err != nil {
 		return nil, err
 	}
 
 	// Create file entry in database
-	file, err := s.createFile(req, upload)
+	file, err := service.createFile(req, upload)
 	if err != nil {
 		app.Log.Errorf("Assembly failed for request %s, couldn't create file in database: %s", req.FlowIdentifier, err)
 		return nil, err
@@ -129,19 +129,19 @@ func (s *uploadService) assemble(req *UploadRequest, dir string) (*schema.File, 
 	// then copy over the data. If it is, then there isn't any uploaded data to copy over.
 	if !upload.IsExisting {
 		// Create on disk entry to write chunks to
-		if err := s.createDest(file.ID); err != nil {
+		if err := service.createDest(file.ID); err != nil {
 			app.Log.Errorf("Assembly failed for request %s, couldn't create file on disk: %s", req.FlowIdentifier, err)
 			return file, err
 		}
 
 		// Move file
-		uploadDir := s.requestPath.dir(req.Request)
-		s.fops.Rename(filepath.Join(uploadDir, req.UploadID()), app.MCDir.FilePath(file.ID))
+		uploadDir := service.requestPath.dir(req.Request)
+		service.fops.Rename(filepath.Join(uploadDir, req.UploadID()), app.MCDir.FilePath(file.ID))
 	}
 
 	// Finish updating the file state.
-	finisher := newFinisher(s.files, s.dirs)
-	checksum := s.determineChecksum(req, upload)
+	finisher := newFinisher(service.files, service.dirs)
+	checksum := service.determineChecksum(req, upload)
 	if err := finisher.finish(req, file.ID, checksum, upload); err != nil {
 		app.Log.Errorf("Assembly failed for request %s, couldn't finish request: %s", req.FlowIdentifier, err)
 		return file, err
@@ -149,16 +149,16 @@ func (s *uploadService) assemble(req *UploadRequest, dir string) (*schema.File, 
 
 	app.Log.Infof("successfully upload fileID %s", file.ID)
 
-	s.cleanupUploadRequest(req.UploadID())
+	service.cleanupUploadRequest(req.UploadID())
 	return file, nil
 }
 
 // createFile creates the database file entry.
-func (s *uploadService) createFile(req *UploadRequest, upload *schema.Upload) (*schema.File, error) {
+func (service *uploadService) createFile(req *UploadRequest, upload *schema.Upload) (*schema.File, error) {
 	file := schema.NewFile(upload.File.Name, upload.ProjectOwner)
 	file.Current = false
 
-	f, err := s.files.Insert(&file, upload.DirectoryID, upload.ProjectID)
+	f, err := service.files.Insert(&file, upload.DirectoryID, upload.ProjectID)
 	app.Log.Infof("Created file %s, in %s %s", f.ID, upload.DirectoryID, upload.ProjectID)
 	if err != nil {
 		return nil, err
@@ -168,15 +168,15 @@ func (s *uploadService) createFile(req *UploadRequest, upload *schema.Upload) (*
 
 // createDest creates the destination file and ensures that the directory
 // path is also created.
-func (s *uploadService) createDest(fileID string) error {
+func (service *uploadService) createDest(fileID string) error {
 	dir := app.MCDir.FileDir(fileID)
-	if err := s.fops.MkdirAll(dir, 0700); err != nil {
+	if err := service.fops.MkdirAll(dir, 0700); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *uploadService) determineChecksum(req *UploadRequest, upload *schema.Upload) string {
+func (service *uploadService) determineChecksum(req *UploadRequest, upload *schema.Upload) string {
 	switch {
 	case upload.IsExisting:
 		// Existing file so use its checksum, no need to compute.
@@ -184,30 +184,30 @@ func (s *uploadService) determineChecksum(req *UploadRequest, upload *schema.Upl
 	case upload.ServerRestarted:
 		// Server was restarted, so checksum state in tracker is wrong. Read
 		// disk file to get the checksum.
-		uploadDir := s.requestPath.dir(req.Request)
+		uploadDir := service.requestPath.dir(req.Request)
 		hash, _ := file.HashStr(md5.New(), filepath.Join(uploadDir, req.UploadID()))
 		return hash
 	default:
 		// Checksum in tracker is correct since its state has been properly
 		// updated as blocks are uploaded.
-		return s.tracker.hash(req.UploadID())
+		return service.tracker.hash(req.UploadID())
 	}
 }
 
 // cleanup is called when an error has occurred. It attempts to clean up
 // the state in the database for this particular entry.
-func (s *uploadService) cleanup(req *UploadRequest, fileID string) error {
-	upload, err := s.uploads.ByID(req.FlowIdentifier)
+func (service *uploadService) cleanup(req *UploadRequest, fileID string) error {
+	upload, err := service.uploads.ByID(req.FlowIdentifier)
 	if err != nil {
 		return err
 	}
-	_, err = s.files.Delete(fileID, upload.DirectoryID, upload.ProjectID)
+	_, err = service.files.Delete(fileID, upload.DirectoryID, upload.ProjectID)
 	return err
 }
 
 //cleanupUploadRequest removes the upload request and file chunks.
-func (s *uploadService) cleanupUploadRequest(uploadID string) {
-	s.tracker.clear(uploadID)
-	s.uploads.Delete(uploadID)
-	s.fops.RemoveAll(app.MCDir.UploadDir(uploadID))
+func (service *uploadService) cleanupUploadRequest(uploadID string) {
+	service.tracker.clear(uploadID)
+	service.uploads.Delete(uploadID)
+	service.fops.RemoveAll(app.MCDir.UploadDir(uploadID))
 }
