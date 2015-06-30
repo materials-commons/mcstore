4772e5475b6bb6cfb9a4a2a3cff203d4533234ae
Glenn Tarcea <glenn.tarcea@gmail.com>
2015-06-29 21:25:24 -0400

    Simplify command to just creating a project. Update the command to use the client API.

--- cmd/mccli/project_create_cmd.go
+++ cmd/mccli/project_create_cmd.go
@@ -8,15 +8,6 @@ import (
 	"github.com/materials-commons/mcstore/cmd/pkg/mc"
 )
 
-// createCommandArgs holds values that won't change and are
-// needed during the upload process.
-type projectCreateCommandArgs struct {
-	projectName   string
-	projectID     string
-	directoryPath string
-	n             int
-}
-
 var (
 	// Command contains the arguments and function for the cli project create command.
 	projectCreateCommand = cli.Command{
@@ -31,73 +22,42 @@ var (
 		},
 		Action: projectCreateCLI,
 	}
-
-	// args contains global values, including arguments from the cli
-	// that are needed to create and upload a project.
-	args projectCreateCommandArgs
 )
 
 // createCLI implements the project create command.
 func projectCreateCLI(c *cli.Context) {
-	if err := validateArgs(c); err != nil {
-		fmt.Println("Invalid arguments:", err)
-		os.Exit(1)
-	}
-
-	p := mc.ProjectDBSpec{
-		Name:      args.projectName,
-		Path:      args.directoryPath,
-		ProjectID: args.projectID,
-	}
-	_, err := mc.ProjectOpener.CreateProjectDB(p)
-	if err != nil {
-		fmt.Println("Unable to create project:", err)
-		os.Exit(1)
-	}
-}
-
-// validate will validate the command line arguments. It will print a message
-// and exit if there is a bad argument.
-func validateArgs(c *cli.Context) error {
 	if len(c.Args()) != 1 {
-		return fmt.Errorf("You must supply a name for the project.")
+		fmt.Println("You must specify a project name")
+		os.Exit(1)
 	}
-	args.projectName = c.Args()[0]
+	projectName := c.Args()[0]
 
-	if err := createProject(args.projectName); err != nil {
-		return err
+	dirPath := c.String("directory")
+	if !validateDirectoryPath(dirPath) {
+		fmt.Println("Invalid directory path: ", dirPath)
+		os.Exit(1)
 	}
 
-	args.directoryPath = c.String("directory")
-	return validateDirectoryPath(args.directoryPath)
-}
+	client := mc.NewClientAPI()
+	if err := client.CreateProject(projectName, dirPath); err != nil {
+		fmt.Println("Unable to create project", err)
+		os.Exit(1)
+	}
 
-// createProject creates the new project for the user.
-func createProject(projectName string) error {
-	//	req := mcstore.CreateProjectRequest{
-	//		Name:         projectName,
-	//		MustNotExist: true,
-	//	}
-	//
-	//	var resp mcstore.CreateProjectResponse
-	//	client := gorequest.New().TLSClientConfig(&tls.Config{InsecureSkipVerify: true})
-	//	if err := sendRequest(client, "/projects", req, &resp); err != nil {
-	//		fmt.Println("Unable to create project:", err)
-	//		return err
-	//	}
-	//
-	//	args.projectID = resp.ProjectID
-	return nil
+	fmt.Println("Project successfully created")
 }
 
 // validateDirectoryPath checks that the given directory path exists.
-func validateDirectoryPath(path string) error {
+func validateDirectoryPath(path string) bool {
 	if path == "" {
-		return fmt.Errorf("You must specify a local directory path where the project files are located.")
+		fmt.Println("You must specify a local directory path where the project files are located.")
+		return false
 	}
 
 	if _, err := os.Stat(path); err != nil {
-		return fmt.Errorf("Invalid directory: %s", path)
+		fmt.Println("Directory doesn't exist or you don't have access")
+		return false
 	}
-	return nil
+
+	return true
 }
