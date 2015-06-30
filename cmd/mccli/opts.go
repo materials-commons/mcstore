f7433f15ce52733569d4c55f5e973a46c369cd78
V. Glenn Tarcea <gtarcea@umich.edu>
2015-06-30 08:40:12 -0400

    Refactor getNumThreads for readability.

--- cmd/mccli/opts.go
+++ cmd/mccli/opts.go
@@ -12,14 +12,15 @@ const maxSimultaneous = 5
 // getNumThreads ensures that the number of parallel downloads is valid.
 func getNumThreads(c *cli.Context) int {
 	numThreads := c.Int("parallel")
-
-	if numThreads < 1 {
+	switch {
+	case numThreads < 1:
 		fmt.Println("Simultaneous downloads must be positive: ", numThreads)
 		os.Exit(1)
-	} else if numThreads > maxSimultaneous {
+	case numThreads > maxSimultaneous:
 		fmt.Printf("You may not set simultaneous downloads greater than %d: %d\n", maxSimultaneous, numThreads)
 		os.Exit(1)
+	default:
+		// Nothing to do.
 	}
-
 	return numThreads
 }
