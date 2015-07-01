e9c0aa1ffb4cfe7b93f64f3c30e909d95b735992
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-01 16:19:41 -0400

    Start moving the api to using retries under the hood.

--- server/mcstore/server_api.go
+++ server/mcstore/server_api.go
@@ -9,6 +9,7 @@ import (
 	"strings"
 
 	"github.com/materials-commons/gohandy/ezhttp"
+	"github.com/materials-commons/gohandy/with"
 	"github.com/materials-commons/mcstore/pkg/app"
 	"github.com/materials-commons/mcstore/pkg/app/flow"
 	"github.com/materials-commons/mcstore/pkg/db/schema"
@@ -19,48 +20,74 @@ import (
 type ServerAPI struct {
 	agent  *gorequest.SuperAgent
 	client *ezhttp.EzClient
+	with.Retrier
 }
 
 // NewServerAPI creates a new ServerAPI
 func NewServerAPI() *ServerAPI {
+	retrier := with.NewRetrier()
+	retrier.RetryCount = 0
 	return &ServerAPI{
-		agent:  gorequest.New().TLSClientConfig(&tls.Config{InsecureSkipVerify: true}),
-		client: MCClient(),
+		agent:   gorequest.New().TLSClientConfig(&tls.Config{InsecureSkipVerify: true}),
+		client:  MCClient(),
+		Retrier: retrier,
 	}
+}
+
+func (s *ServerAPI) SetRetrier(r with.Retrier) {
+	s.Retrier = r
 }
 
 // CreateUploadRequest will request an upload request from the server. If an existing
 // upload matches the request then server will send the existing upload request.
 func (s *ServerAPI) CreateUploadRequest(req CreateUploadRequest) (*CreateUploadResponse, error) {
-	var uploadResponse CreateUploadResponse
-	sc, err := s.client.JSON(&req).JSONPost(Url("/upload"), &uploadResponse)
-	if err != nil {
-		return nil, err
+	var (
+		statusCode     int
+		err            error
+		uploadResponse CreateUploadResponse
+	)
+	fn := func() bool {
+		if statusCode, err = s.client.JSON(&req).JSONPost(Url("/upload"), &uploadResponse); err != nil {
+			return false
+		} else {
+			err = HTTPStatusToError(statusCode)
+			return true
+		}
 	}
-	err = HTTPStatusToError(sc)
-	if err != nil {
-		return nil, err
-	}
+	s.WithRetry(fn)
 	return &uploadResponse, nil
 }
 
 // SendFlowData will send the data for a flow request.
 func (s *ServerAPI) SendFlowData(req *flow.Request) (*UploadChunkResponse, error) {
-	params := req.ToParamsMap()
-	sc, err, body := s.client.PostFileBytes(Url("/upload/chunk"), "/tmp/test.txt", "chunkData",
-		req.Chunk, params)
-	switch {
-	case err != nil:
-		return nil, err
-	case sc != 200:
-		return nil, app.ErrInternal
-	default:
-		var uploadResp UploadChunkResponse
-		if err := ToJSON(body, &uploadResp); err != nil {
-			return nil, err
+	var (
+		body       string
+		statusCode int
+		err        error
+		uploadResp UploadChunkResponse
+	)
+	fn := func() bool {
+		params := req.ToParamsMap()
+		statusCode, err, body = s.client.PostFileBytes(Url("/upload/chunk"), "/tmp/test.txt", "chunkData",
+			req.Chunk, params)
+		if err != nil {
+			return false
 		}
-		return &uploadResp, nil
+		switch {
+		case statusCode != 200:
+			err = app.ErrInternal
+		default:
+			var uploadResp UploadChunkResponse
+			err = ToJSON(body, &uploadResp)
+		}
+		return true
 	}
+
+	s.WithRetry(fn)
+	if err != nil {
+		return nil, err
+	}
+	return &uploadResp, nil
 }
 
 // ListUploadRequests will return all the upload requests for a given project ID.
