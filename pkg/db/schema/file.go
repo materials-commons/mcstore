64cec84b7c4127762b31b2cb98c8146a70bb0d30
V. Glenn Tarcea <gtarcea@umich.edu>
2015-07-30 14:07:03 -0400

    Move elastic search items out of schema and put into an override definition in mcbulk.go

--- pkg/db/schema/file.go
+++ pkg/db/schema/file.go
@@ -36,25 +36,22 @@ type MediaType struct {
 // File models a user file. A datafile is an abstract representation of a real file
 // plus the attributes that we need in our model for access, and other metadata.
 type File struct {
-	ID          string    `gorethink:"id,omitempty" json:"id"`                           // Primary key.
-	Type        string    `gorethink:"_type" json:"_type"`                               // Type
-	Current     bool      `gorethink:"current" json:"current"`                           // Is this the most current version.
-	Name        string    `gorethink:"name" json:"name"`                                 // Name of file.
-	Path        string    `gorethink:"path,omitempty" json:"path"`                       // Directory path where file resides.
-	Birthtime   time.Time `gorethink:"birthtime" json:"birthtime"`                       // Creation time.
-	MTime       time.Time `gorethink:"mtime" json:"mtime"`                               // Modification time.
-	ATime       time.Time `gorethink:"atime" json:"atime"`                               // Last access time.
-	Description string    `gorethink:"description" json:"description"`                   // Description of file
-	MediaType   MediaType `gorethink:"mediatype" json:"mediatype"`                       // File media type and description
-	Owner       string    `gorethink:"owner" json:"owner"`                               // Who owns the file.
-	Checksum    string    `gorethink:"checksum" json:"checksum"`                         // MD5 Hash.
-	Size        int64     `gorethink:"size" json:"size"`                                 // Size of file.
-	Uploaded    int64     `gorethink:"uploaded" json:"-"`                                // Number of bytes uploaded. When Size != Uploaded file is only partially uploaded.
-	Parent      string    `gorethink:"parent" json:"parent"`                             // If there are multiple ids then parent is the id of the previous version.
-	UsesID      string    `gorethink:"usesid" json:"usesid"`                             // If file is a duplicate, then usesid points to the real file. This allows multiple files to share a single physical file.
-	DataDirID   string    `gorethink:"datadir_id,omitempty" json:"datadir_id,omitempty"` // Directory file is located in
-	ProjectID   string    `gorethink:"project_id,omitempty" json:"project_id,omitempty"` // Project file is in
-	Contents    string    `gorethink:"-" json:"contents"`                                // Contents of the file (text only)
+	ID          string    `gorethink:"id,omitempty" json:"id"`         // Primary key.
+	Type        string    `gorethink:"_type" json:"_type"`             // Type
+	Current     bool      `gorethink:"current" json:"current"`         // Is this the most current version.
+	Name        string    `gorethink:"name" json:"name"`               // Name of file.
+	Path        string    `gorethink:"path,omitempty" json:"path"`     // Directory path where file resides.
+	Birthtime   time.Time `gorethink:"birthtime" json:"birthtime"`     // Creation time.
+	MTime       time.Time `gorethink:"mtime" json:"mtime"`             // Modification time.
+	ATime       time.Time `gorethink:"atime" json:"atime"`             // Last access time.
+	Description string    `gorethink:"description" json:"description"` // Description of file
+	MediaType   MediaType `gorethink:"mediatype" json:"mediatype"`     // File media type and description
+	Owner       string    `gorethink:"owner" json:"owner"`             // Who owns the file.
+	Checksum    string    `gorethink:"checksum" json:"checksum"`       // MD5 Hash.
+	Size        int64     `gorethink:"size" json:"size"`               // Size of file.
+	Uploaded    int64     `gorethink:"uploaded" json:"-"`              // Number of bytes uploaded. When Size != Uploaded file is only partially uploaded.
+	Parent      string    `gorethink:"parent" json:"parent"`           // If there are multiple ids then parent is the id of the previous version.
+	UsesID      string    `gorethink:"usesid" json:"usesid"`           // If file is a duplicate, then usesid points to the real file. This allows multiple files to share a single physical file.
 }
 
 // NewFile creates a new File instance.
