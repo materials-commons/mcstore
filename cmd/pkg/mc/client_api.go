9eff643a40e144a8fffaa280baeba1ac672c0ccf
V. Glenn Tarcea <gtarcea@umich.edu>
2015-12-04 10:01:49 -0500

    Add method documentation.

--- cmd/pkg/mc/client_api.go
+++ cmd/pkg/mc/client_api.go
@@ -8,11 +8,14 @@ import (
 	"github.com/materials-commons/mcstore/server/mcstore"
 )
 
+// ClientAPI implements API calls to the mcstored server.
 type ClientAPI struct {
 	serverAPI     *mcstore.ServerAPI
 	projectOpener ProjectDBOpener
 }
 
+// NewClientAPI creates a new instance of a ClientAPI. It checks for client side project
+// data in $HOME/.materialscommons
 func NewClientAPI() *ClientAPI {
 	return &ClientAPI{
 		serverAPI:     mcstore.NewServerAPI(),
@@ -20,6 +23,8 @@ func NewClientAPI() *ClientAPI {
 	}
 }
 
+// newClientAPIWithConfiger creates a new instance of a ClientAPI. The calls supplies the
+// configer used to find the client side project database.
 func newClientAPIWithConfiger(configer Configer) *ClientAPI {
 	opener := sqlProjectDBOpener{
 		configer: configer,
@@ -30,14 +35,19 @@ func newClientAPIWithConfiger(configer Configer) *ClientAPI {
 	}
 }
 
-func (c *ClientAPI) UploadFile(projectID string, path string) error {
+// UploadFile uploads a single file to the given project.
+func (c *ClientAPI) UploadFile(projectName string, path string) error {
 	return nil
 }
 
-func (c *ClientAPI) UploadDirectory(projectID string, path string) error {
+// UploadDirectory uploads all the entries in a given directory. It will
+// not follow sub directories. However it will create sub directories
+// that are direct children of the given path.
+func (c *ClientAPI) UploadDirectory(projectName string, path string) error {
 	return nil
 }
 
+// UploadProject will upload all the changed and new files in a given project.
 func (c *ClientAPI) UploadProject(projectName string, numThreads int) error {
 	if projectDB, err := ProjectOpener.OpenProjectDB(projectName); err != nil {
 		return err
@@ -50,10 +60,13 @@ func (c *ClientAPI) UploadProject(projectName string, numThreads int) error {
 	}
 }
 
+//
 func (c *ClientAPI) ProjectStatus(projectID string) error {
 	return nil
 }
 
+// CreateProject creates a new project. If the project already
+// exists on the client it returns app.ErrExists.
 func (c *ClientAPI) CreateProject(name, path string) error {
 	if c.projectOpener.ProjectExists(name) {
 		return app.ErrExists
@@ -75,6 +88,8 @@ func (c *ClientAPI) CreateProject(name, path string) error {
 	}
 }
 
+// CreateProjectDirectories will create all the directories on the server
+// that are found under the client project.
 func (c *ClientAPI) CreateProjectDirectories(projectName string) error {
 	projectDB, err := ProjectOpener.OpenProjectDB(projectName)
 	if err != nil {
@@ -92,6 +107,7 @@ func (c *ClientAPI) CreateProjectDirectories(projectName string) error {
 	return nil
 }
 
+// CreateDirectory will create a single directory on the server for the named project.
 func (c *ClientAPI) CreateDirectory(projectName, path string) (string, error) {
 	projectDB, err := c.projectOpener.OpenProjectDB(projectName)
 	if err != nil {
@@ -101,6 +117,7 @@ func (c *ClientAPI) CreateDirectory(projectName, path string) (string, error) {
 	return c.createDir(projectDB.Project(), path)
 }
 
+// createDir performs the actual call to the server to create a directory.
 func (c *ClientAPI) createDir(project *Project, path string) (directoryID string, err error) {
 	req := mcstore.DirectoryRequest{
 		ProjectName: project.Name,
@@ -111,10 +128,12 @@ func (c *ClientAPI) createDir(project *Project, path string) (directoryID string
 	return c.serverAPI.GetDirectory(req)
 }
 
+// RenameProject will rename an existing project.
 func (c *ClientAPI) RenameProject(oldName, newName string) error {
 	return nil
 }
 
+// IndexProject will index a project (whatever that means at the moment)
 func (c *ClientAPI) IndexProject(projectName string) error {
 	return nil
 }
