81dbdfa8c8fb1e2c167bc02f541eb77cebb0ffd5
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-01-19 12:10:29 -0500

    Add additional client side errors.

--- cmd/pkg/mc/errors.go
+++ cmd/pkg/mc/errors.go
@@ -2,4 +2,8 @@ package mc
 
 import "errors"
 
-var ErrInvalidProjectFilePath = errors.New("path not in project")
+var (
+	ErrInvalidProjectFilePath = errors.New("path not in project")
+	ErrFileNotUploaded        = errors.New("existing file not uploaded")
+	ErrFileVersionNotUploaded = errors.New("existing file with new version not uploaded")
+)
