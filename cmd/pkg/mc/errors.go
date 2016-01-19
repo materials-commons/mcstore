8e54018c90eec6bf7218262ff54454c6e93415e0
V. Glenn Tarcea <glenn.tarcea@gmail.com>
2016-01-19 13:48:32 -0500

    Make ErrFileVersionNotUploaded error message more understandable.

--- cmd/pkg/mc/errors.go
+++ cmd/pkg/mc/errors.go
@@ -5,5 +5,5 @@ import "errors"
 var (
 	ErrInvalidProjectFilePath = errors.New("path not in project")
 	ErrFileNotUploaded        = errors.New("existing file not uploaded")
-	ErrFileVersionNotUploaded = errors.New("existing file with new version not uploaded")
+	ErrFileVersionNotUploaded = errors.New("existing file has changes that haven't been uploaded")
 )
