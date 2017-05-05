3a8a9186ffda579ab3e2810ba3d454bdcd0a4909
V. Glenn Tarcea <gtarcea@umich.edu>
2017-05-05 11:42:29 -0400

    Explore new permissions system

--- pkg/db/schema/access.go
+++ pkg/db/schema/access.go
@@ -24,3 +24,54 @@ func NewAccess(projectID, projectName, userID string) Access {
 		UserID:      userID,
 	}
 }
+
+type AccessPermission string
+
+const (
+	PermReadWrite       AccessPermission = "RW"
+	PermRead            AccessPermission = "R"
+	PermReadWriteDelete AccessPermission = "RWD"
+)
+
+type Access2 struct {
+	ID          string           `gorethink:"id,omitempty"`
+	Owner       string           `gorethink:"owner"`
+	GroupName   string           `gorethink:"group_name"`
+	Permissions AccessPermission `gorethink:"permissions"`
+	Users       []string         `gorethink:"users"`
+	Items       []Access2Item    `gorethink:"items"`
+}
+
+type Access2Item struct {
+	ID       string `gorethink:"id,omitempty"`
+	ItemType string `gorethink:"item_type"`
+	ItemID   string `gorethink:"item_id"`
+}
+
+func Owner(owner string) func(a *Access2) {
+	return func(a *Access2) {
+		a.Owner = owner
+	}
+}
+
+func Users(users ...string) func(a *Access2) {
+	return func(a *Access2) {
+		a.Users = append(a.Users, users...)
+	}
+}
+
+func Permission(perm AccessPermission) func(a *Access2) {
+	return func(a *Access2) {
+		a.Permissions = perm
+	}
+}
+
+func PermittedItem(itemID, itemType string) func(a *Access2) {
+	return func(a *Access2) {
+		a.Items = append(a.Items, Access2Item{ItemID: itemID, ItemType: itemType})
+	}
+}
+
+func NewAccess2(owner string, name string) {
+
+}
